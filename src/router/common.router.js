39e230b29c5e1d8c7c567249fe148a23e2530353
xiewenli <1223260658@qq.com>
2022-12-20 23:04:37 +0800

    修改、添加数据统计echarts的请求

--- src/router/common.router.js
+++ src/router/common.router.js
@@ -1,11 +1,17 @@
 const Router = require("koa-router");
 
-const { updateAllStatus } = require("../controller/common.controller");
+const {
+  updateAllStatus,
+  getStatus,
+} = require("../controller/common.controller");
 const { verifyAuth, verifyPower } = require("../middleware/auth.middleware");
 
 const commonRouter = new Router({ prefix: "/common" });
 
 // 修改common的所有状态其中之一
 commonRouter.get("/updateAllStatus", verifyAuth, verifyPower, updateAllStatus);
+
+// 获取common表数据
+commonRouter.get("/getStatus", verifyAuth, verifyPower, getStatus);
 
 module.exports = commonRouter;
