3edc5b4580a1fefbfed23bd35e68a9a1797d675d
xiewenli <1223260658@qq.com>
2023-01-13 13:51:16 +0800

    版本1.2.1（开放静态资源）

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,8 @@
 const koa = require("koa");
 const app = new koa();
 
+const serve = require("koa-static");
+const path = require("path");
 const cors = require("@koa/cors");
 const bodyparser = require("koa-bodyparser");
 
@@ -11,6 +13,9 @@ const errFn = require("./errFn");
 app.use(cors());
 // 请求体解析
 app.use(bodyparser());
+
+// 开放静态文件
+app.use(serve(path.resolve(__dirname, "../../public/")));
 
 // 路由
 useRouter(app);
