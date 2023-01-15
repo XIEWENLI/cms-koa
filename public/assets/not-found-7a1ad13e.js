54edfacfd43689fabf7239bd8e0c309cc6d298e4
xiewenli <1223260658@qq.com>
2023-01-16 00:22:16 +0800

    撤销静态资源

--- public/assets/not-found-7a1ad13e.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as t,b as c}from"./index-ccedb79d.js";const n={},s=c("h1",null,"NOT FOUND",-1),_=[s];function r(a,d){return o(),t("div",null,_)}const f=e(n,[["render",r]]);export{f as default};
